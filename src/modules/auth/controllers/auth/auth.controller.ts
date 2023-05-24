aa95645cdeba413d2c7b9611ad66c3361b197e7f
dimahavryliak <havryliakdmytro@gmail.com>
2023-05-24 23:04:47 +0300

    add signup

--- src/modules/auth/controllers/auth/auth.controller.ts
+++ src/modules/auth/controllers/auth/auth.controller.ts
@@ -3,17 +3,25 @@ import { AuthService } from '../../providers/auth/auth.service';
 import { AuthLoginDto } from './dto/login.dto';
 import { AuthGuard } from '../../providers/guard/auth.guard';
 import { User } from 'src/models/user.model';
+import { AuthSignUpDto } from './dto/signup.dto';
+import { UsersService } from 'src/modules/database/providers/users/users.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(
-    private authService: AuthService
+    private authService: AuthService, 
+    private usersService: UsersService,
   ) { }
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() body: AuthLoginDto) {
     return this.authService.signIn(body.email, body.password);
+  }
+
+  @Post('signup')
+  async signUp(@Body() body: AuthSignUpDto){
+    await this.usersService.addUser(body)
   }
 
   @UseGuards(AuthGuard)
--- src/modules/database/providers/tags/tags.service.ts
+++ src/modules/database/providers/tags/tags.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { Tag } from "src/models/tag.model";
-import { DataSource, Repository } from "typeorm";
+import { DataSource, In, Repository } from "typeorm";
 
 @Injectable()
 export class TagsService {
-    private repository: Repository<Tag>;
+  private repository: Repository<Tag>;
 
-    constructor(dataSource: DataSource) {
-      this.repository = dataSource.getRepository(Tag);
-    }
+  constructor(dataSource: DataSource) {
+    this.repository = dataSource.getRepository(Tag);
+  }
 
-    async getByName(name: string) {
-        return await this.repository.findOneBy({ name })
-    }
+  async getByName(name: string) {
+    return await this.repository.findOneBy({ name })
+  }
 
+  async getMultipleByName(names: string[]) {
+    return await this.repository.findBy({ name: In(names) })
+  }
 }
